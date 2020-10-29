29f9e7673d5305910179a6f3680b6bda929c0669
Kristian <kristiangcorrea@live.com>
2020-10-28 21:01:51 -0400

    Neated everything up. Fixed validation.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 
   const handleChange = event => {
     setUrl(event.target.value)
+    setShortLink(false)
   }
 
   const handleSubmit = event => {
@@ -45,18 +46,25 @@ function App() {
   
   let shortUrl;
   if (shortLink){
-    shortUrl = <div><input style={{width: `11%`}} readonly type="text" id="shortUrl" value = {`https://link-itt.herokuapp.com/${url.shortID}`} /><button onClick= {copyText}>Copy text</button></div>
+    shortUrl = 
+    <div>
+      <h3>Here's your tinyer link.</h3>
+      <input style={{width: `21%`, fontSize: "1.5rem"}} readonly type="text" id="shortUrl" value = {`https://link-itt.herokuapp.com/${url.shortID}`} /><button style={{fontSize: "1.5rem", fontFamily: "sans-serif"}}onClick= {copyText}>Copy to Clipboard</button>
+    </div>
   } else {
   }
   return (
     <div className="App">
         <img src={logo} style={{width: "10%"}}className="" alt="logo" />
+        <h2>Turn your long links into tiny links!</h2>
         <form action="" onSubmit={handleSubmit}>
-          <input style= {{width: "30%"}} type="url" name="url" id="url" onChange={handleChange}></input>
-          <button type="submit">link it</button>
+          <input style= {{width: "30%", fontSize: "1.2rem", textAlign: "center"}} required placeholder="Insert long URL here" type="url" name="url" id="url" onChange={handleChange}></input>
+          <br/>
+          <button style={{marginTop: "1%", fontSize: "1.2rem"}}type="submit">Squeeze Link!</button>
         </form>
         
         <br/>
+        
         {shortUrl}
     </div>
   );
