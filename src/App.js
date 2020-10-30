4a183c47a9fcbd7c379237662bd92f85db0d44ad
Kristian <kristiangcorrea@live.com>
2020-10-30 12:30:55 -0400

    Added a table for previously created links.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import logo from './logo.png';
 import './App.css';
 import axios from 'axios';
 import Loader from 'react-loader-spinner'
-
+const currentAppUrl = `http://localhost:5000/`
 function App() {
   const [url, setUrl] = useState("")
   const [shortLink, setShortLink] = useState(false)
   const [generatingLink, setGeneratingLink] = useState(false)
+  const [userLinks, setUserLinks] = useState([])
+
+  useEffect(() => {
+    axios.get(`${currentAppUrl}userLinks`)
+      .then(res => {
+        setUserLinks(res.data)
+      })
+  }, [])
+  useEffect(() => {
+    axios.get(`${currentAppUrl}userLinks`)
+      .then(res => {
+        setUserLinks(res.data)
+      })
+  }, [shortLink])
 
   const handleChange = event => {
     setUrl(event.target.value)
@@ -22,7 +36,7 @@ function App() {
     };
     setGeneratingLink(true);
     setTimeout(() => {  console.log("World!"); }, 4000);
-    axios.post(`https://link-itt.herokuapp.com/`, data)
+    axios.post(`${currentAppUrl}`, data)
       .then(res => {
         console.log(res.data[0]);
         setUrl(res.data[0])
@@ -54,8 +68,8 @@ function App() {
     shortUrl = 
     <div>
       <h3 style={{fontWeight: "normal"}}>Here's your tinyer link.</h3>
-      <a href={`https://link-itt.herokuapp.com/${url.shortID}`} >
-        <input style={{width: `21%`, fontSize: "1.5rem", textDecoration: "underline", color: "blue"}} readOnly type="text" id="shortUrl" value = {`https://link-itt.herokuapp.com/${url.shortID}`} />
+      <a href={`${currentAppUrl}${url.shortID}`} >
+        <input style={{width: `21%`, fontSize: "1.5rem", textDecoration: "underline", color: "blue"}} readOnly type="text" id="shortUrl" value = {`${currentAppUrl}${url.shortID}`} />
       </a>
       <button class="copy" style={{fontSize: "1.2rem", fontFamily: "sans-serif"}} onClick={copyText}>Copy</button>
     </div>
@@ -74,9 +88,36 @@ function App() {
         <br/>
         
         {shortUrl}
+        {userLinks.length == 0 ? null : <UserLinks userLinks = {userLinks}/>}
     </div>
   );
 }
 
+function UserLinks(props) {
+  const userLinks = props.userLinks
+  return(
+    <div>
+      <table align="center">
+        <tr>
+          <th>Short URL</th> |
+          <th>Full URL</th> |
+          <th>Visits</th>
+        </tr>
+        {userLinks.map((shortUrl) => {
+          return(
+          <tr>
+            <th><a href={`${currentAppUrl}${shortUrl.shortID}`}>{currentAppUrl}{shortUrl.shortID}</a></th> |
+            <th><a href={`${shortUrl.url}`}>{shortUrl.url}</a></th> |
+            <th>{shortUrl.visits}</th>
+          </tr>
+
+            )
+        })}
+        
+      </table>
+      
+    </div>
+  )
+}
 
 export default App;
